3d8c42a760f7065dce6a764056d0e004052ce4e6
sumitmahajan <sumit@stagwell.tech>
2017-08-15 16:23:35 -0400

    New Commit

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
+import json
+
+CONFIG_FILE = os.environ['CONFIG_FILE'] if 'CONFIG_FILE' in os.environ else 'config/parallel.json'
+TASK_ID = int(os.environ['TASK_ID']) if 'TASK_ID' in os.environ else 0
+
+with open(CONFIG_FILE) as data_file:
+    CONFIG = json.load(data_file)
 
 
 def before_feature(context, feature):
+    desired_capabilities = CONFIG['environments'][2]
+
+    for key in CONFIG["capabilities"]:
+        if key not in desired_capabilities:
+            desired_capabilities[key] = CONFIG["capabilities"][key]
+
     context.browser = webdriver.Remote(
         command_executor='http://sumitmahajan1:Cgvydx3ZyqJBboxGTqWy@hub.browserstack.com:80/wd/hub',
-        desired_capabilities=DesiredCapabilities.FIREFOX)
+        desired_capabilities=desired_capabilities)
 
 
 def after_feature(context, feature):
