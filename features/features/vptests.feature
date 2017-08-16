efbf9a4d6cdf9cd200e44feaea3e760f5202a198
sumitmahajan <sumit@stagwell.tech>
2017-08-16 14:05:20 -0400

    New Commit

--- features/features/vptests.feature
+++ features/features/vptests.feature
@@ -1,4 +1,11 @@
 Feature: Victory Passport Donation Page
+
   Scenario: I can see victory passport footer on Victory Passport Donation Page
     When I have vp site open
     Then I can see footer Paid for by Stripe AAN
+
+  Scenario: I can contribute on victory passport page
+    Given I have vp site open
+    When I fill all fields on vp page
+    And I click on contribute button
+    Then I can make contribution
--- features/steps/vpsteps.py
+++ features/steps/vpsteps.py
@@ -2,14 +2,17 @@ from behave import Given,When,Then
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from features.helpers.config import Config as cf
+import logging
+from time import sleep
+log = logging.getLogger(__name__)
 
 
 class VpSteps:
+
     @When("I have vp site open")
     def step(context):
         base_url = cf.base_url
         context.browser.get(base_url)
-
 
     @Then("I can see footer Paid for by Stripe AAN")
     def step(context):
@@ -17,4 +20,27 @@ class VpSteps:
         elem = context.browser.find_element_by_class_name("paid-for-by-container").text
         assert "Paid for by Stripe AAN" == elem.strip()
 
+    @When("I fill all fields on vp page")
+    def step(context):
+        context.browser.implicitly_wait(5)
+        context.browser.switch_to.frame(context.browser.find_element_by_tag_name("iframe"))
+        elem = context.browser.find_element_by_xpath("//span[text()='$4']")
+        elem.click()
+        sleep(1)
+        elem = context.browser.find_element_by_css_selector('#first-name')
+        elem.send_keys("Sumit")
+        sleep(1)
+        elem = context.browser.find_element_by_css_selector('#last-name')
+        elem.send_keys("Mahajan")
+        sleep(1)
+        elem = context.browser.find_element_by_css_selector('#street-address')
+        elem.send_keys("33 Sheppard Ave E")
+        sleep(1)
+        elem = context.browser.find_element_by_css_selector('#apartment')
+        elem.send_keys("501")
+        sleep(5)
+        #context.browser.switch_to.default_content()
 
+    @Given("I have vp site open")
+    def step(context):
+        log.info(context.browser.title)
